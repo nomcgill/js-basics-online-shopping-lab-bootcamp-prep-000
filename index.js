fea437fe0c94bf55de9aa07c63de44be6e6e851e
Nolan McGill <nomcgill@gmail.com>
2018-09-22 01:57:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -70,10 +70,13 @@ function removeFromCart(item) {
 
 
 function placeOrder(cardNumber) {
-  if 
-  
-  return `Sorry, we don't have a credit card on file for you.`
+  if (cardNumber === undefined) {
+    return (`Sorry, we don't have a credit card on file for you.`);
+  } else {
+    return (`Your total cost is ${totalValue()}, which will be charged to the card ${cardNumber}.`);
+    cart = [];
+    return cart;
+  }
 }
-
 
 
